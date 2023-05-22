34ac6d574ba8cc6a1f324eec007e588d8be18af5
mariamrevia <mariam.revia@redberry.ge>
2023-05-22 23:43:17 +0400

    refactor:modified router index.js

--- src/router/index.js
+++ src/router/index.js
@@ -5,18 +5,14 @@ import IdentficationView from "@/views/IdentificationView.vue"
 import CovidQuestionView from "@/views/CovidQuestionView.vue"
 import VaccinationView from "@/views/VaccinationView.vue"
 
-const routes = [
-  {path:'/' , component:StartingPageView},
-  {path:'/identification' , component:IdentficationView},
-  {path:'/covidquestion' , component:CovidQuestionView},
-  {path:'/vaccination' , component:VaccinationView},
-  
-  
-]
 const router = createRouter({
-  history:createWebHistory(),
-  routes:routes
-})
+  history: createWebHistory(),
+  routes: [
+    { path: '/', component: StartingPageView },
+    { path: '/identification', component: IdentficationView },
+    { path: '/covidquestion', component: CovidQuestionView },
+    { path: '/vaccination', component: VaccinationView }
+  ]
+});
 
-
-export default router
+export default router;
