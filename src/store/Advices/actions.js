1a4eefce18b45f4cc460328f2f4752418b449c28
mariamrevia <mariam.revia@redberry.ge>
2023-05-31 10:48:16 +0400

    refactor:extracted fucntion from async function

--- src/store/Advices/actions.js
+++ src/store/Advices/actions.js
@@ -1,22 +1,20 @@
 import { sendData } from '@/services/api/form.js'
 import router from '@/router/index.js'
 
+function isEmpty(object) {
+  if (object === '') return true
+  if (typeof object !== 'object') return false
+  for (let key in object) {
+    if (!isEmpty(object[key])) return false
+  }
+  return true
+}
 export default {
   updateAdvicesData({ commit }, payload) {
-    console.log(payload)
     commit('updateAdvice', payload)
   },
 
   async sendDatatoAPI(_, payload) {
-    function isEmpty(obj) {
-      if (obj === '') return true
-      if (typeof obj !== 'object') return false
-      for (let key in obj) {
-        if (!isEmpty(obj[key])) return false
-      }
-      return true
-    }
-
     const filteredPayload = Object.fromEntries(
       Object.entries(payload).filter((entry) => !isEmpty(entry[1]))
     )
