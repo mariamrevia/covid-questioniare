4542c6ea6a09545b12db4f1b347aec6d16b15728
mariamrevia <mariam.revia@redberry.ge>
2023-05-31 10:50:46 +0400

    refactor:delete console.logs

--- src/store/Identification/mutations.js
+++ src/store/Identification/mutations.js
@@ -1,6 +1,5 @@
 export default {
   updateData(state, payload) {
-    console.log(payload)
     state.identificationData[payload.name] = payload.value
     localStorage.setItem('identificationData', JSON.stringify(state))
   }
