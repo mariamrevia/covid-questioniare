759926bb9677c7e16833a48ebd54a814189478a9
mariamrevia <mariam.revia@redberry.ge>
2023-05-31 11:13:30 +0400

    refactor:delete consoles

--- src/store/vaccination/mutations.js
+++ src/store/vaccination/mutations.js
@@ -1,6 +1,5 @@
 export default {
   updateVaccinationData(state, payload) {
-    console.log(payload)
     state.vaccinationData[payload.name] = payload.value
     localStorage.setItem('vaccinationData', JSON.stringify(state))
   }
